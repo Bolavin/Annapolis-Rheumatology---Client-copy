6a61aaab57d2165b23f5a72ad3b9a083dc17f58a
Bolavin <bolavinraj@gmail.com>
2024-11-15 10:30:30 -0800

    Fixed some bugs and in tratment and make the social link work too..

--- js/play.js
+++ js/play.js
@@ -1,14 +1,22 @@
-input.onfocus = function (){
-    datalist.style.display = 'block';
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.btn--filter');
+    const containerWrapper = document.querySelector('.conditions__wrapper');
+    const containers = containerWrapper.querySelectorAll('.condition-block');
 
-for (let option of datalist.options) {
-    option.onclick = function (){
-        input.value = this.value;
-        datalist.style.display = 'none';
-    }
-}
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            // Remove 'active' class from all buttons
+            buttons.forEach(btn => btn.classList.remove('active'));
 
-// datalist.style.width = input.offsetWidth + 'px';
-// datalist.style.left = input.offsetLeft + 'px';
-// datalist.style.top = input.offsetTop + input.offsefHeight + 'px';
+            // Add 'active' class to the clicked button
+            button.classList.add('active');
+
+            // Add 'is-hidden' class to all containers
+            containers.forEach(container => container.classList.add('is-hidden'));
+
+            // Remove 'is-hidden' class from the corresponding container
+            const containerId = button.id;
+            containerWrapper.querySelector(`#${containerId}`).classList.remove('is-hidden');
+        });
+    });
+});
